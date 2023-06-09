1f2d7700c6d6aa2d0c00ae64d123e48e72c055be
Taoista <taoista-@hotmail.com>
2023-06-09 04:40:53 -0400

    inicio del repositorio con vistual studio 2015

---
+++ ConectorHuellero/Form1.vb
@@ -0,0 +1,48 @@
+﻿Imports DPFP
+Imports DPFP.Capture
+Public Class Main
+    Implements DPFP.Capture.EventHandler
+
+    Private Captura As DPFP.Capture.Capture
+
+    ' verifica al inciiar la capura
+    Protected Overridable Sub Init()
+        Try
+            Captura = New Capture()
+            If Not Captura Is Nothing Then
+                Captura.EventHandler = Me
+            Else
+                MessageBox.Show("no se pudo iniciar al captura")
+            End If
+        Catch ex As Exception
+            MessageBox.Show("No se iniciar la camptura")
+        End Try
+    End Sub
+
+
+
+
+    Public Sub OnComplete(Capture As Object, ReaderSerialNumber As String, Sample As Sample) Implements EventHandler.OnComplete
+
+    End Sub
+
+    Public Sub OnFingerGone(Capture As Object, ReaderSerialNumber As String) Implements EventHandler.OnFingerGone
+
+    End Sub
+
+    Public Sub OnFingerTouch(Capture As Object, ReaderSerialNumber As String) Implements EventHandler.OnFingerTouch
+
+    End Sub
+
+    Public Sub OnReaderConnect(Capture As Object, ReaderSerialNumber As String) Implements EventHandler.OnReaderConnect
+
+    End Sub
+
+    Public Sub OnReaderDisconnect(Capture As Object, ReaderSerialNumber As String) Implements EventHandler.OnReaderDisconnect
+
+    End Sub
+
+    Public Sub OnSampleQuality(Capture As Object, ReaderSerialNumber As String, CaptureFeedback As CaptureFeedback) Implements EventHandler.OnSampleQuality
+
+    End Sub
+End Class
